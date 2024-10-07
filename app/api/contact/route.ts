5f82ca651b0748f21e8419c7cd98625cf68f7f35
nahsooyeon <nahsooyeon@rapidglobal.co.kr>
2024-10-07 14:43:14 +0900

    feat: Contact API 추가

---
+++ app/api/contact/route.ts
@@ -0,0 +1,31 @@
+// app/api/contact/route.ts
+import { NextResponse } from "next/server";
+
+import { Resend } from "resend";
+
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
+
+const resend = new Resend(RESEND_API_KEY);
+
+export async function POST(req: Request) {
+  try {
+    const { email, subject, content } = await req.json();
+
+    if (!email || !subject || !content) {
+      return NextResponse.json({ error: "Missing fields" }, { status: 400 });
+    }
+
+    // Resend API를 통해 이메일 발송
+    await resend.emails.send({
+      from: email,
+      to: ["owner@pomme.dev"],
+      subject: subject,
+      html: content,
+    });
+
+    return NextResponse.json({ message: "Email sent successfully" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Failed to send email" }, { status: 500 });
+  }
+}
